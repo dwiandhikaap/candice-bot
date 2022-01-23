9af5e0469c362a0709113286099aee83c2515d4a
Dwi Andhika <dwiandhikaaji@gmail.com>
2022-01-23 20:37:28 +0700

    Added `/jadwal` command, but still not functional

--- commands/Jadwal.js
+++ commands/Jadwal.js
@@ -3,12 +3,16 @@ const { JadwalEmbed } = require("../util/CommandEmbed");
 const jadwalData = require("../res/jadwal.json");
 
 /**
-* @param {CommandInteraction} interaction - User interaction
-*/
-async function jadwal(interaction){
-    interaction.reply(JadwalEmbed(jadwalData));
+ * @param {CommandInteraction} interaction - User interaction
+ */
+async function jadwal(interaction) {
+    // TODO: use proper userConcentration on the next semester (semester 4)
+    const userConcentration = "all";
+    const semester = 3;
+
+    interaction.reply(JadwalEmbed(jadwalData, semester, userConcentration));
 }
 
 module.exports = {
-    jadwal : jadwal
-}
+    jadwal: jadwal,
+};
