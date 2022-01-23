ec8d090ecdbd42b8d74c2b25bf6c9f6ab6bfa220
Dwi Andhika <dwiandhikaaji@gmail.com>
2022-01-23 19:00:51 +0700

    renamed the hash function

--- util/DatabaseHandler/UserAuthHandler.js
+++ util/DatabaseHandler/UserAuthHandler.js
@@ -1,62 +1,63 @@
-const { encrypt, hashUserId, decrypt } = require("../Encryption");
+const { encrypt, hash, decrypt } = require("../Encryption");
 const { db } = require("./MainDatabase");
 
 var credentials = db.collection("credential");
 
-async function dbGetData(userid){
-    const key = hashUserId(userid,process.env.KEYSALT);
-    const id = hashUserId(userid,process.env.IDSALT);
-    const userData = await credentials.findOne({"userid" : id});
+async function dbGetData(userid) {
+    const key = hash(userid, process.env.KEYSALT);
+    const id = hash(userid, process.env.IDSALT);
+    const userData = await credentials.findOne({ userid: id });
 
-    if(userData){
+    if (userData) {
         userData.nim = decrypt(key, userData.nim);
         userData.password = decrypt(key, userData.password);
-    };
+    }
 
     return userData;
 }
 
-async function dbSearch(id){
+async function dbSearch(id) {
     const user = await dbGetData(id);
-    if(user){
+    if (user) {
         return true;
-    }
-    else{
+    } else {
         return false;
     }
 }
 
-async function dbInsert(id, nim, password){
-    const key = hashUserId(id,process.env.KEYSALT);
+async function dbInsert(id, nim, password) {
+    const key = hash(id, process.env.KEYSALT);
     await credentials.insertOne({
-        "userid" : hashUserId(id,process.env.IDSALT),
-        "nim" : encrypt(key,nim),
-        "password" : encrypt(key,password)
+        userid: hash(id, process.env.IDSALT),
+        nim: encrypt(key, nim),
+        password: encrypt(key, password),
     });
 }
 
-async function dbUpdate(userid, nim, password){
-    const key = hashUserId(userid,process.env.KEYSALT);
-    const id = hashUserId(userid,process.env.IDSALT);
-    await credentials.replaceOne({"userid" : id} ,{
-        "userid" : id,
-        "nim" : encrypt(key,nim),
-        "password" : encrypt(key,password)
-    });
+async function dbUpdate(userid, nim, password) {
+    const key = hash(userid, process.env.KEYSALT);
+    const id = hash(userid, process.env.IDSALT);
+    await credentials.replaceOne(
+        { userid: id },
+        {
+            userid: id,
+            nim: encrypt(key, nim),
+            password: encrypt(key, password),
+        }
+    );
 }
 
-async function dbDelete(id){
+async function dbDelete(id) {
     // Just in case there are duplicates, so people don't sue me
     await credentials.deleteMany({
-        "userid" : hashUserId(id,process.env.IDSALT)
+        userid: hash(id, process.env.IDSALT),
     });
 }
 
 module.exports = {
-    dbGetData : dbGetData,
-    dbSearch : dbSearch,
-    dbInsert : dbInsert,
-    dbUpdate : dbUpdate,
-    dbDelete : dbDelete,
-}
-
+    dbGetData: dbGetData,
+    dbSearch: dbSearch,
+    dbInsert: dbInsert,
+    dbUpdate: dbUpdate,
+    dbDelete: dbDelete,
+};
--- util/Encryption.js
+++ util/Encryption.js
@@ -1,36 +1,39 @@
-const { createHash, createCipheriv, randomBytes, createDecipheriv } = require('crypto');
+const { createHash, createCipheriv, randomBytes, createDecipheriv } = require("crypto");
 
-function hashUserId(userid, salt){
-    return createHash('sha256').update(userid+salt).digest('hex').substr(0,32);
+function hash(userid, salt) {
+    return createHash("sha256")
+        .update(userid + salt)
+        .digest("hex")
+        .substr(0, 32);
 }
 
-function encrypt(key, msg){
+function encrypt(key, msg) {
     const iv = randomBytes(16);
-    const cipher = createCipheriv('aes-256-gcm', key, iv);
-    const encMsg = cipher.update(msg, 'utf8');
+    const cipher = createCipheriv("aes-256-gcm", key, iv);
+    const encMsg = cipher.update(msg, "utf8");
     const enc = cipher.final();
     const tag = cipher.getAuthTag();
-    
-    return Buffer.concat([encMsg, enc, iv, tag]).toString('hex');
+
+    return Buffer.concat([encMsg, enc, iv, tag]).toString("hex");
 }
 
-function decrypt(key, str){
-    str = Buffer.from(str, 'hex');
+function decrypt(key, str) {
+    str = Buffer.from(str, "hex");
 
-    const msg = str.slice(0,str.length-32);
-    const iv = str.slice(str.length-32, str.length-16);
-    const tag = str.slice(str.length-16);
-    const decipher = createDecipheriv('aes-256-gcm', key, iv);
+    const msg = str.slice(0, str.length - 32);
+    const iv = str.slice(str.length - 32, str.length - 16);
+    const tag = str.slice(str.length - 16);
+    const decipher = createDecipheriv("aes-256-gcm", key, iv);
 
     decipher.setAuthTag(tag);
     let dencMsg = decipher.update(msg);
-    dencMsg += decipher.final('utf8');
+    dencMsg += decipher.final("utf8");
 
     return dencMsg;
 }
 
 module.exports = {
-    hashUserId : hashUserId,
-    encrypt : encrypt,
-    decrypt : decrypt
-}
+    hash: hash,
+    encrypt: encrypt,
+    decrypt: decrypt,
+};
