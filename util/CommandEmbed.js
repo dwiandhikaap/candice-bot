d8d230119fe38a780256d2fd785a9f1a1092f6ec
Dwi Andhika <dwiandhikaaji@gmail.com>
2022-01-24 20:02:55 +0700

    adjusted `JadwalEmbed` arguments
    also some code formatting cuz prettier

--- util/CommandEmbed.js
+++ util/CommandEmbed.js
@@ -1,87 +1,117 @@
-const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js")
-const { parseMakul, parseKhs, parseTranskrip, generateJadwalField } = require("./Util")
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
+const { parseMakul, parseKhs, parseTranskrip, generateJadwalField } = require("./Util");
 
-function CommandInfoEmbed(param){
-    return {embeds : [{
-            "title": param.title,
-            "color": "#4278f5",
-            "description": `${param.desc} \n\n**How to use**\n\`${param.syntax}\`\n\n**Example**\n\`${param.example}\``
-          }]}
+function CommandInfoEmbed(param) {
+    return {
+        embeds: [
+            {
+                title: param.title,
+                color: "#4278f5",
+                description: `${param.desc} \n\n**How to use**\n\`${param.syntax}\`\n\n**Example**\n\`${param.example}\``,
+            },
+        ],
+    };
 }
 
-function NotifEmbed(param){
-    return {embeds : [{
-            "title": param.title,
-            "color": "#4278f5",
-            "description": `${param.desc}`
-          }]}
+function NotifEmbed(param) {
+    return {
+        embeds: [
+            {
+                title: param.title,
+                color: "#4278f5",
+                description: `${param.desc}`,
+            },
+        ],
+    };
 }
 
-function InvalidAcademicYear(){
-    return {embeds : [{
-      "color": "#4278f5",
-      "description": `Invalid academic year!. Valid example : \`2020/2021\``
-    }]}
+function InvalidAcademicYear() {
+    return {
+        embeds: [
+            {
+                color: "#4278f5",
+                description: `Invalid academic year!. Valid example : \`2020/2021\``,
+            },
+        ],
+    };
 }
 
-function InvalidToken(){
-  return {embeds : [{
-    "color": "#4278f5",
-    "description": `Invalid token!. Valid example : \`abC12\``
-  }]}
+function InvalidToken() {
+    return {
+        embeds: [
+            {
+                color: "#4278f5",
+                description: `Invalid token!. Valid example : \`abC12\``,
+            },
+        ],
+    };
 }
 
-function UserNotFound(){
-  return {embeds : [{
-    "color": "#4278f5",
-    "description": `No account found. Register via \`/register [NIM] [Password]\``
-  }]}
+function UserNotFound() {
+    return {
+        embeds: [
+            {
+                color: "#4278f5",
+                description: `No account found. Register via \`/register [NIM] [Password]\``,
+            },
+        ],
+    };
 }
 
-function AuthFailed(){
-  return {embeds : [{
-    "color": "#4278f5",
-    "description": "Authentication failed! Please do `/register` with your correct username and password!"
-  }]}
+function AuthFailed() {
+    return {
+        embeds: [
+            {
+                color: "#4278f5",
+                description: "Authentication failed! Please do `/register` with your correct username and password!",
+            },
+        ],
+    };
 }
 
-function PresensiEmbed(isSuccess){
-  return {embeds : [{
-    "color": "#4278f5",
-    "description": isSuccess ? "Presensi Sukses ✅" : "Presensi Gagal ❌",
-    "timestamp": new Date()
-  }]}
+function PresensiEmbed(isSuccess) {
+    return {
+        embeds: [
+            {
+                color: "#4278f5",
+                description: isSuccess ? "Presensi Sukses ✅" : "Presensi Gagal ❌",
+                timestamp: new Date(),
+            },
+        ],
+    };
 }
 
-function UserProfileEmbed(commandData){
-  const {user, mhsData} = commandData;
-  const {Nama, Npm, Prodi, Angkatan, IsAktif} = mhsData.Mhs
-  const {TahunAkademik, Semester} = mhsData.PeriodeAkademik
+function UserProfileEmbed(commandData) {
+    const { user, mhsData } = commandData;
+    const { Nama, Npm, Prodi, Angkatan, IsAktif } = mhsData.Mhs;
+    const { TahunAkademik, Semester } = mhsData.PeriodeAkademik;
 
-  const embed = new MessageEmbed()
+    const embed = new MessageEmbed()
         .setThumbnail(user.displayAvatarURL())
         .setTitle(`Profile ▸ _**${user.username}**_`)
         .setColor("#4278f5")
-        .setDescription(`
+        .setDescription(
+            `
             Nama : **${Nama}**
             NIM : **${Npm}**
-        `)
+        `
+        )
         .addFields(
-          {name: "Program Studi", value: `▸ ${Prodi}`, inline: true},
-          {name: "Angkatan", value: `▸ ${Angkatan}`, inline: true},
-          {name: "Status", value: `${IsAktif ? '✅   Aktif' : '❌   Tidak Aktif'}`, inline: true}
+            { name: "Program Studi", value: `▸ ${Prodi}`, inline: true },
+            { name: "Angkatan", value: `▸ ${Angkatan}`, inline: true },
+            { name: "Status", value: `${IsAktif ? "✅   Aktif" : "❌   Tidak Aktif"}`, inline: true }
         )
         .setFooter(`${TahunAkademik} - Semester ${Semester == 1 ? "Ganjil" : "Genap"}`)
         .setTimestamp(new Date());
 
-  return {embeds : [embed]}
+    return { embeds: [embed] };
 }
 
-function UserMakulEmbed(param){
-  const {user, buttonIdTag, userMakulData, isOddSemester, tahunAkademik} = param;
-  const makulparsed = parseMakul(userMakulData);
+function UserMakulEmbed(param) {
+    const { user, buttonIdTag, userMakulData, isOddSemester, tahunAkademik } = param;
+    const makulparsed = parseMakul(userMakulData);
 
-  const embed = new MessageEmbed()
+    const embed = new MessageEmbed()
         .setThumbnail(user.displayAvatarURL())
         .setTitle(`Mata Kuliah ▸ _**${user.username}**_`)
         .setColor("#4278f5")
@@ -89,157 +119,155 @@ function UserMakulEmbed(param){
         .setTimestamp(new Date())
         .setFooter(`${tahunAkademik} - Semester ${isOddSemester ? "Ganjil" : "Genap"}`);
 
-  const buttonRow = new MessageActionRow().addComponents(
-      new MessageButton()
-          .setStyle(isOddSemester ? 3 : 2)
-          .setLabel('Ganjil')
-          .setCustomId('oddBtn'+buttonIdTag)
-          .setDisabled(isOddSemester),
+    const buttonRow = new MessageActionRow().addComponents(
+        new MessageButton()
+            .setStyle(isOddSemester ? 3 : 2)
+            .setLabel("Ganjil")
+            .setCustomId("oddBtn" + buttonIdTag)
+            .setDisabled(isOddSemester),
 
-      new MessageButton()
-          .setStyle(isOddSemester? 2 : 3)
-          .setLabel('Genap')
-          .setCustomId('evenBtn'+buttonIdTag)
-          .setDisabled(!isOddSemester)
-  )
-    
-  return {embeds : [embed], components: [buttonRow]}
+        new MessageButton()
+            .setStyle(isOddSemester ? 2 : 3)
+            .setLabel("Genap")
+            .setCustomId("evenBtn" + buttonIdTag)
+            .setDisabled(!isOddSemester)
+    );
+
+    return { embeds: [embed], components: [buttonRow] };
 }
 
-function UserKhsEmbed(param){
-  const { user, buttonIdTag, userKhsData, isOddSemester, tahunAkademik} = param;
-  const { IpkSem, JmlSks } = userKhsData;
-  const khsparsed = parseKhs(userKhsData);
+function UserKhsEmbed(param) {
+    const { user, buttonIdTag, userKhsData, isOddSemester, tahunAkademik } = param;
+    const { IpkSem, JmlSks } = userKhsData;
+    const khsparsed = parseKhs(userKhsData);
 
-  const embed = new MessageEmbed()
+    const embed = new MessageEmbed()
         .setThumbnail(user.displayAvatarURL())
         .setTitle(`Info KHS ▸ _**${user.username}**_`)
         .setColor("#4278f5")
         .addFields(
-          {name: ':medal: Index Prestasi', value: `▸ **${IpkSem.toFixed(2)}** / 4.00`, inline:true},
-          {name: ':books: Jumlah SKS', value: `▸ **${JmlSks}** SKS`, inline:true}
-          )
-          .addField('\u200B', khsparsed)
-          .setFooter(`${tahunAkademik} - Semester ${isOddSemester ? "Ganjil" : "Genap"}`)
-          .setTimestamp(new Date());
-        
-        
-  const buttonRow = new MessageActionRow().addComponents(
-      new MessageButton()
-          .setStyle(isOddSemester ? 3 : 2)
-          .setLabel('Ganjil')
-          .setCustomId('oddBtn'+buttonIdTag)
-          .setDisabled(isOddSemester),
+            { name: ":medal: Index Prestasi", value: `▸ **${IpkSem.toFixed(2)}** / 4.00`, inline: true },
+            { name: ":books: Jumlah SKS", value: `▸ **${JmlSks}** SKS`, inline: true }
+        )
+        .addField("\u200B", khsparsed)
+        .setFooter(`${tahunAkademik} - Semester ${isOddSemester ? "Ganjil" : "Genap"}`)
+        .setTimestamp(new Date());
 
-      new MessageButton()
-          .setStyle(isOddSemester? 2 : 3)
-          .setLabel('Genap')
-          .setCustomId('evenBtn'+buttonIdTag)
-          .setDisabled(!isOddSemester)
-  )
-    
-  return {embeds : [embed], components: [buttonRow]}
+    const buttonRow = new MessageActionRow().addComponents(
+        new MessageButton()
+            .setStyle(isOddSemester ? 3 : 2)
+            .setLabel("Ganjil")
+            .setCustomId("oddBtn" + buttonIdTag)
+            .setDisabled(isOddSemester),
+
+        new MessageButton()
+            .setStyle(isOddSemester ? 2 : 3)
+            .setLabel("Genap")
+            .setCustomId("evenBtn" + buttonIdTag)
+            .setDisabled(!isOddSemester)
+    );
+
+    return { embeds: [embed], components: [buttonRow] };
 }
 
-function UserTranskripEmbed(param){
-  const { user, buttonIdTag, transkripData, currentPage, pages, indexRange } = param;
-  const transkripParsed = parseTranskrip(transkripData, indexRange);
-  const { Ipk, JmlSks, SksWajib, SksKonsentrasi, SksPilihan } = transkripData;
+function UserTranskripEmbed(param) {
+    const { user, buttonIdTag, transkripData, currentPage, pages, indexRange } = param;
+    const transkripParsed = parseTranskrip(transkripData, indexRange);
+    const { Ipk, JmlSks, SksWajib, SksKonsentrasi, SksPilihan } = transkripData;
 
-  const embed = new MessageEmbed()
+    const embed = new MessageEmbed()
         .setThumbnail(user.displayAvatarURL())
         .setTitle(`Info Transkrip ▸ _**${user.username}**_`)
         .setColor("#4278f5")
         .addFields(
-          {name: ':medal: IPK', value: `▸ **${Ipk.toFixed(2)}** / 4.00`, inline:true},
-          {name: ':books: Jumlah SKS', value: `▸ **${JmlSks}** SKS`, inline:true},
-          {name: '\u200B', value: '\u200B', inline:true},
-          {name: ':green_book: SKS Wajib', value: `▸ **${SksWajib}** SKS`, inline:true},
-          {name: ':orange_book: SKS Konsentrasi', value: `▸ **${SksKonsentrasi}** SKS`, inline:true},
-          {name: ':bookmark: SKS Pilihan', value: `▸ **${SksPilihan}** SKS`, inline:true}
-          )
-          .addField('\u200B', transkripParsed)
-          .setFooter(`Page ${currentPage}/${pages}`)
-          .setTimestamp(new Date());
-        
-        
-  const buttonRow = new MessageActionRow().addComponents(
-      new MessageButton()
-          .setStyle(1)
-          .setLabel('Prev. Page')
-          .setCustomId('prevBtn'+buttonIdTag)
-          .setDisabled(currentPage == 1),
+            { name: ":medal: IPK", value: `▸ **${Ipk.toFixed(2)}** / 4.00`, inline: true },
+            { name: ":books: Jumlah SKS", value: `▸ **${JmlSks}** SKS`, inline: true },
+            { name: "\u200B", value: "\u200B", inline: true },
+            { name: ":green_book: SKS Wajib", value: `▸ **${SksWajib}** SKS`, inline: true },
+            { name: ":orange_book: SKS Konsentrasi", value: `▸ **${SksKonsentrasi}** SKS`, inline: true },
+            { name: ":bookmark: SKS Pilihan", value: `▸ **${SksPilihan}** SKS`, inline: true }
+        )
+        .addField("\u200B", transkripParsed)
+        .setFooter(`Page ${currentPage}/${pages}`)
+        .setTimestamp(new Date());
 
-      new MessageButton()
-          .setStyle(1)
-          .setLabel('Next Page')
-          .setCustomId('nextBtn'+buttonIdTag)
-          .setDisabled(currentPage == pages)
-  )
-    
-  return {embeds : [embed], components: [buttonRow]}
+    const buttonRow = new MessageActionRow().addComponents(
+        new MessageButton()
+            .setStyle(1)
+            .setLabel("Prev. Page")
+            .setCustomId("prevBtn" + buttonIdTag)
+            .setDisabled(currentPage == 1),
+
+        new MessageButton()
+            .setStyle(1)
+            .setLabel("Next Page")
+            .setCustomId("nextBtn" + buttonIdTag)
+            .setDisabled(currentPage == pages)
+    );
+
+    return { embeds: [embed], components: [buttonRow] };
 }
 
-function InfoEmbed(nodeJsVersion, discordJsVersion, mongoDBInfo, botInfo, clientAvatarUrl){
-  const embed = new MessageEmbed()
+function InfoEmbed(nodeJsVersion, discordJsVersion, mongoDBInfo, botInfo, clientAvatarUrl) {
+    const embed = new MessageEmbed()
         .setThumbnail(clientAvatarUrl)
         .setColor("#4278f5")
-        .addFields({
-          "name": `📝 About Candice`,
-          "value": `[candice](https://gitfront.io/r/user-6093614/0784dcf390539e7ac2345a1e5ece5fed49904d8c/candice-bot/) is a Discord Bot that was built to allow users to access AMIKOM One features without having to own an Android device. This project is currently a private repository maintained by [siveroo](https://github.com/siveroo).`
-        },
-        {
-          "name": `🤖 Bot Info`,
-          "value": `Version: ${botInfo.version}\nUptime: ${botInfo.uptime}`,
-          "inline": true
-        },
-        {
-          "name": `<:nodejs:893466618217238539> Node.js Version`,
-          "value": nodeJsVersion,
-          "inline": true
-        },
-        {
-          "name": `<:discordjs:893466628703010876> Discord.js Version`,
-          "value": discordJsVersion,
-          "inline": true
-        },
-				{
-          "name": `<:mongodb:895298927459442808> MongoDB Server Info`,
-          "value": `Version: ${mongoDBInfo.version}\nUptime: ${mongoDBInfo.uptime}\n`,
-					"inline": true
-        }
+        .addFields(
+            {
+                name: `📝 About Candice`,
+                value: `[candice](https://gitfront.io/r/user-6093614/0784dcf390539e7ac2345a1e5ece5fed49904d8c/candice-bot/) is a Discord Bot that was built to allow users to access AMIKOM One features without having to own an Android device. This project is currently a private repository maintained by [siveroo](https://github.com/siveroo).`,
+            },
+            {
+                name: `🤖 Bot Info`,
+                value: `Version: ${botInfo.version}\nUptime: ${botInfo.uptime}`,
+                inline: true,
+            },
+            {
+                name: `<:nodejs:893466618217238539> Node.js Version`,
+                value: nodeJsVersion,
+                inline: true,
+            },
+            {
+                name: `<:discordjs:893466628703010876> Discord.js Version`,
+                value: discordJsVersion,
+                inline: true,
+            },
+            {
+                name: `<:mongodb:895298927459442808> MongoDB Server Info`,
+                value: `Version: ${mongoDBInfo.version}\nUptime: ${mongoDBInfo.uptime}\n`,
+                inline: true,
+            }
         )
-        .setFooter(`Made by siveroo`, 'https://avatars.githubusercontent.com/u/53227252?v=4')
+        .setFooter(`Made by siveroo`, "https://avatars.githubusercontent.com/u/53227252?v=4");
 
-  return {embeds : [embed]}
+    return { embeds: [embed] };
 }
 
+function JadwalEmbed(jadwalData, semester, userConcentration) {
+    const jadwalFields = generateJadwalField(jadwalData, semester, userConcentration);
+    const embed = new MessageEmbed()
+        .setColor("#4278f5")
+        .setTitle("Jadwal Kuliah ▸ **20 BCI 01**")
+        .setFooter("2021/2022 - Semester Ganjil")
+        .setTimestamp(new Date())
+        .addFields(jadwalFields);
 
-function JadwalEmbed(jadwalData){
-	const jadwalFields = generateJadwalField(jadwalData);
-	const embed = new MessageEmbed()
-		.setColor("#4278f5")
-		.setTitle("Jadwal Kuliah ▸ **20 BCI 01**")
-		.setFooter("2021/2022 - Semester Ganjil")
-		.setTimestamp(new Date())
-		.addFields(jadwalFields);
-	
-	return {embeds : [embed]};
+    return { embeds: [embed] };
 }
 
 module.exports = {
-    CommandInfoEmbed : CommandInfoEmbed,
-    NotifEmbed : NotifEmbed,
-    UserNotFound : UserNotFound,
-    AuthFailed : AuthFailed,
-    InvalidAcademicYear : InvalidAcademicYear,
-    InvalidToken : InvalidToken,
-    
-    InfoEmbed : InfoEmbed,
-    PresensiEmbed : PresensiEmbed,
-    UserProfileEmbed : UserProfileEmbed,
-    UserMakulEmbed : UserMakulEmbed,
-    UserKhsEmbed : UserKhsEmbed,
-    UserTranskripEmbed : UserTranskripEmbed,
-		JadwalEmbed : JadwalEmbed
-}
+    CommandInfoEmbed: CommandInfoEmbed,
+    NotifEmbed: NotifEmbed,
+    UserNotFound: UserNotFound,
+    AuthFailed: AuthFailed,
+    InvalidAcademicYear: InvalidAcademicYear,
+    InvalidToken: InvalidToken,
+
+    InfoEmbed: InfoEmbed,
+    PresensiEmbed: PresensiEmbed,
+    UserProfileEmbed: UserProfileEmbed,
+    UserMakulEmbed: UserMakulEmbed,
+    UserKhsEmbed: UserKhsEmbed,
+    UserTranskripEmbed: UserTranskripEmbed,
+    JadwalEmbed: JadwalEmbed,
+};
--- util/Util.js
+++ util/Util.js
@@ -1,110 +1,108 @@
 function isInvalidYear(year) {
-	return !(
-		/^\d{4}\/\d{4}$/i.test(year) &&
-		parseInt(year.split("/")[1]) - parseInt(year.split("/")[0]) === 1 &&
-		parseInt(year.split("/")[0]) <= new Date().getFullYear()
-	);
+    return !(
+        /^\d{4}\/\d{4}$/i.test(year) &&
+        parseInt(year.split("/")[1]) - parseInt(year.split("/")[0]) === 1 &&
+        parseInt(year.split("/")[0]) <= new Date().getFullYear()
+    );
 }
-	
+
 function isInvalidToken(token) {
-	return token.length != 5;
+    return token.length != 5;
 }
 
 function parseMakul(makulData) {
-	let result = "";
-	if (Object.keys(makulData).length == 0) {
-		return ":warning:  **No data found!**";
-	}
-	
-	for (const makul of makulData) {
-		const { Kode, NamaMk, JmlSks, JmlPresensiKuliah, IsHadirMID, IsHadirUAS } =
-		makul;
-		
-		result += `
+    let result = "";
+    if (Object.keys(makulData).length == 0) {
+        return ":warning:  **No data found!**";
+    }
+
+    for (const makul of makulData) {
+        const { Kode, NamaMk, JmlSks, JmlPresensiKuliah, IsHadirMID, IsHadirUAS } = makul;
+
+        result += `
 		**• ${NamaMk} - ${Kode}** - *${JmlSks} SKS*
 		▸ Presensi  : **${JmlPresensiKuliah}**
 		▸ Hadir UTS : **${IsHadirMID ? "Ya" : "Tidak"}**
 		▸ Hadir UAS : **${IsHadirUAS ? "Ya" : "Tidak"}**
 		`;
-	}
-	return result;
+    }
+    return result;
 }
 
 function parseKhs(khsData) {
-	const { Khs } = khsData;
-	let result = "";
-	
-	if (Object.keys(Khs).length == 0) {
-		return ":warning:  **No data found!**";
-	}
-	
-	for (const infoKhs of Khs) {
-		const { Kode, NamaMk, JmlSks, Nilai } = infoKhs;
-		
-		result += `
+    const { Khs } = khsData;
+    let result = "";
+
+    if (Object.keys(Khs).length == 0) {
+        return ":warning:  **No data found!**";
+    }
+
+    for (const infoKhs of Khs) {
+        const { Kode, NamaMk, JmlSks, Nilai } = infoKhs;
+
+        result += `
 		**• ${NamaMk} - ${Kode}** - *${JmlSks} SKS*
 		▸ Nilai  : **${Nilai === null ? "Belum ada data!" : Nilai}**
 		`;
-	}
-	return result;
+    }
+    return result;
 }
 
 function parseTranskrip(transkripData, indexRange) {
-	const { Transkrip } = transkripData;
-	let result = "";
-	
-	if (Object.keys(Transkrip).length == 0) {
-		return ":warning:  **No data found!**";
-	}
-	
-	for (let i = indexRange[0]; i < indexRange[1]; i++) {
-		const { Kode, NamaMk, JmlSks, Nilai, NamaSifatMk } = Transkrip[i];
-		
-		result += `
+    const { Transkrip } = transkripData;
+    let result = "";
+
+    if (Object.keys(Transkrip).length == 0) {
+        return ":warning:  **No data found!**";
+    }
+
+    for (let i = indexRange[0]; i < indexRange[1]; i++) {
+        const { Kode, NamaMk, JmlSks, Nilai, NamaSifatMk } = Transkrip[i];
+
+        result += `
 		**• ${NamaMk} - ${Kode}** - *${JmlSks} SKS*
 		▸ Nilai  : **${Nilai === null ? "Belum ada data!" : Nilai}**
 		▸ Tipe  : **${NamaSifatMk === null ? "Belum ada data!" : NamaSifatMk}**
 		`;
-	}
-	
-	return result;
+    }
+
+    return result;
 }
 
-function generateJadwalField(jadwalData) {
-	const jadwalField = [];
+function generateJadwalField(jadwalData, semester, userConcentration) {
+    const jadwalField = [];
 
-	for (jadwal of jadwalData) {
-		const { hari, data } = jadwal;
-		let jadwalValue = "";
-		const jadwalHarian = {
-			name: `**${hari}**`,
-			inline: true
-		}
+    const jadwalUser = jadwalData[semester][userConcentration];
 
-		if(Object.keys(data).length == 0){
-			jadwalValue = "-";
-		}
+    for (jadwal of jadwalUser) {
+        const { hari, data } = jadwal;
+        let jadwalValue = "";
+        const jadwalHarian = {
+            name: `**${hari}**`,
+            inline: true,
+        };
 
-		else{
-			for(makul of data){
-				const {name, startTime, endTime} = makul;
-				jadwalValue += `# ${name}\n${startTime} - ${endTime} WIB\n\n`;
-			}
-		}
+        if (Object.keys(data).length == 0) {
+            jadwalValue = "-";
+        } else {
+            for (makul of data) {
+                const { name, startTime, endTime } = makul;
+                jadwalValue += `# ${name}\n${startTime} - ${endTime} WIB\n\n`;
+            }
+        }
 
+        jadwalHarian.value = `\`\`\`md\n${jadwalValue}\`\`\``;
+        jadwalField.push(jadwalHarian);
+    }
 
-		jadwalHarian.value = `\`\`\`md\n${jadwalValue}\`\`\``;
-		jadwalField.push(jadwalHarian);
-	}
-
-	return jadwalField;
+    return jadwalField;
 }
 
 module.exports = {
-	isInvalidYear: isInvalidYear,
-	isInvalidToken: isInvalidToken,
-	parseMakul: parseMakul,
-	parseKhs: parseKhs,
-	parseTranskrip: parseTranskrip,
-	generateJadwalField: generateJadwalField
+    isInvalidYear: isInvalidYear,
+    isInvalidToken: isInvalidToken,
+    parseMakul: parseMakul,
+    parseKhs: parseKhs,
+    parseTranskrip: parseTranskrip,
+    generateJadwalField: generateJadwalField,
 };
